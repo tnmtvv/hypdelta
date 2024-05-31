f476eebb382ada61192150319c223c6935bbe60a
TanyaMatveeva <62231627+tnmtvv@users.noreply.github.com>
2024-05-31 09:32:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,30 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+# Read the contents of your README file
+from pathlib import Path
 
-from setuptools import setup, find_packages
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="hyppy",
+    name="hypdelta",
     version="0.1.0",
     author="Tanya Matveeva",
     author_email="tpmatveeva@hse.ru",
-    description="A Python library for computing hyperbolic delta values from distance matrices",
+    description="A Python library for calculating delta hyperbolicity.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/tnmtvv/hyppy",
-    project_urls={
-        "Bug Tracker": "https://github.com/tnmtvv/hyppy/issues",
-    },
+    url="https://github.com/tnmtvv/hypdelta",
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=find_packages(),
-    python_requires=">=3.9",
-    install_requires=["numpy", "numba", "scipy", "matplotlib"],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy>=1.18.0",
+        "numba>=0.48.0",
+        "scipy>=1.4.0",
+    ],
 )
