b050e8586b6946b83000018597be84ce71365777
TanyaMatveeva <62231627+tnmtvv@users.noreply.github.com>
2024-05-31 10:07:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tnmtvv/hypdelta",
-    packages=find_packages(),
+    packages=["src\hypdelta", "tests"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
