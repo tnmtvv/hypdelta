d3af06c72a2be40f3be2c7949cb81b1a491972a8
TanyaMatveeva <62231627+tnmtvv@users.noreply.github.com>
2024-05-31 09:34:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.9",
     install_requires=[
         "numpy>=1.18.0",
         "numba>=0.48.0",
